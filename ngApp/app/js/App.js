433a1f2f2dec4b35ae9d6cfe7fa452c5467913a7
Andrew Mikhailov <amikhailov@adyax.com>
2016-04-18 23:42:35 +0600

    Added Angular app

---
+++ ngApp/app/js/App.js
@@ -0,0 +1,18 @@
+(function (angular, global) {
+  'use strict';
+
+  angular
+    .module('application', [
+      'ngRoute',
+      'ngAnimate',
+      'ngSanitize',
+      'angular-carousel-3d',
+      'angular-preload-image',
+      'slick',
+      "com.2fdevs.videogular",
+      "com.2fdevs.videogular.plugins.controls",
+      "com.2fdevs.videogular.plugins.overlayplay",
+      "com.2fdevs.videogular.plugins.poster"
+    ]);
+  }
+)(angular, window);
---
+++ ngApp/app/js/LoginPageController.js
@@ -0,0 +1,9 @@
+(function (angular, global) {
+  'use strict';
+
+  angular.module('application')
+    .controller('LoginPageController', ['$scope', '$log', function ($scope, $log) {
+      var ctrl = this;
+
+    }])
+})(angular, window);
