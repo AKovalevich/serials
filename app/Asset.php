348fd9d1c95d6dbce2ac1cadbeda472dfff9f038
Andrew Mikhailov <amikhailov@adyax.com>
2016-04-18 23:33:56 +0600

    Created models Video, Image, Episodes and updated Asset. Create migrations.

--- app/Asset.php
+++ app/Asset.php
@@ -10,7 +10,14 @@ class Asset extends Model
         return $this->belongsToMany('App\Tag', 'asset_tag', 'asset_id', 'tag_id');
     }
 
-    public function genre() {
+    public function genre()
+    {
         return $this->belongsToMany('App\Genre', 'asset_genre', 'asset_id', 'genre_id');
     }
+
+    public function images()
+    {
+        return $this->hasOne('App\Image', 'id');
+    }
+
 }
