5ad8087f65cd33f3e70578713b043a1593f0d0ad
Kovalevich <artemkovalevich00@gmail.com>
2016-05-02 23:44:35 +0600

    hotfix

--- admin/app/Helpers/Format.php
+++ admin/app/Helpers/Format.php
@@ -4,6 +4,9 @@ namespace App\Helpers;
 
 class Format {
     public static function formatBytes($bytes, $precision = 2) {
+        if (!isset($bytes)) {
+            return '';
+        }
         $unit = ["B", "KB", "MB", "GB"];
         $exp = floor(log($bytes, 1024)) | 0;
         return round($bytes / (pow(1024, $exp)), $precision).$unit[$exp];
