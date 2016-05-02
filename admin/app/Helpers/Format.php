ec7a6ff9fdfd755a4314fc3cbb24bea17358edb7
Kovalevich <artemkovalevich00@gmail.com>
2016-05-02 23:49:31 +0600

    hotfix

--- admin/app/Helpers/Format.php
+++ admin/app/Helpers/Format.php
@@ -3,11 +3,30 @@
 namespace App\Helpers;
 
 class Format {
-    public static function formatBytes($bytes, $precision = 2) {
-        $unit = array('B','KB','MB','GB','TB','PB','EB');
+    public static function formatBytes($bytes, $unit = "", $decimals = 2) {
+        $units = array('B' => 0, 'KB' => 1, 'MB' => 2, 'GB' => 3, 'TB' => 4,
+            'PB' => 5, 'EB' => 6, 'ZB' => 7, 'YB' => 8);
 
-        return @round(
-                $bytes / pow(1024, ($i = floor(log($bytes, 1024)))), $precision
-            ) . ' ' . $unit[$i];
+        $value = 0;
+        if ($bytes > 0) {
+            // Generate automatic prefix by bytes
+            // If wrong prefix given
+            if (!array_key_exists($unit, $units)) {
+                $pow = floor(log($bytes) / log(1024));
+                $unit = array_search($pow, $units);
+            }
+
+            // Calculate byte value by prefix
+            $value = ($bytes / pow(1024, floor($units[$unit])));
+        }
+
+        // If decimals is not numeric or decimals is less than 0
+        // then set default value
+        if (!is_numeric($decimals) || $decimals < 0) {
+            $decimals = 2;
+        }
+
+        // Format output
+        return sprintf('%.' . $decimals . 'f '.$unit, $value);
     }
 }
