1902c721994dcc0920c598dd901c3980fe177aef
Kovalevich <artemkovalevich00@gmail.com>
2016-05-02 23:25:55 +0600

    hotfix

--- admin/app/Helpers/Format.php
+++ admin/app/Helpers/Format.php
@@ -5,9 +5,12 @@ namespace App\Helpers;
 class Format {
     public static function formatBytes($size, $precision = 2)
     {
-        $base = log($size, 1024);
-        $suffixes = array('', 'K', 'M', 'G', 'T');
+        $units = array('B', 'KB', 'MB', 'GB', 'TB');
 
-        return round(pow(1024, $base - floor($base)), $precision) .' '. $suffixes[floor($base)];
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow = min($pow, count($units) - 1);
+
+        return round($bytes, $precision) . ' ' . $units[$pow];
     }
 }
