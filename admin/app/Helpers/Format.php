de101f93933c41ee88dc5da2338846bb2412c817
Kovalevich <artemkovalevich00@gmail.com>
2016-05-02 23:00:35 +0600

    System dashboard, some css fixes

---
+++ admin/app/Helpers/Format.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Helpers;
+
+class Format {
+    public static function formatBytes($size, $precision = 2)
+    {
+        $base = log($size, 1024);
+        $suffixes = array('', 'K', 'M', 'G', 'T');
+
+        return round(pow(1024, $base - floor($base)), $precision) .' '. $suffixes[floor($base)];
+    }
+}
