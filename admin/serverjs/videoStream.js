1d4a772912ce4a7b24f73a67b1d3dc2a6a67859f
Andrew Mikhailov <andrew.mikhailov.18.01@gmail.com>
2016-05-11 23:28:13 +0600

    Add videoStream in nodeJS

--- admin/serverjs/videoStream.js
+++ admin/serverjs/videoStream.js
@@ -25,4 +25,4 @@ http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'video/mp4' });
     fs.createReadStream(path).pipe(res);
   }
-}).listen(1337, '127.0.0.1');
+}).listen(1337);
