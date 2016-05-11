d092fa47d520d8d23a5f251504d7e6b0ccc41066
Andrew Mikhailov <amikhailov@adyax.com>
2016-05-11 23:39:21 +0600

    Add cluster.

--- admin/serverjs/videoStream.js
+++ admin/serverjs/videoStream.js
@@ -1,28 +1,36 @@
 var http = require('http'),
   fs = require('fs'),
+  cluster = require('cluster'),
   util = require('util');
 
-http.createServer(function (req, res) {
-  var path = '../storage/app/public/videos/1-Welcome%20to%20Republic%20City.%20A%20Leaf%20in%20the%20Wind.mp4';
-  var stat = fs.statSync(path);
-  var total = stat.size;
-  if (req.headers['range']) {
-    var range = req.headers.range;
-    var parts = range.replace(/bytes=/, "").split("-");
-    var partialstart = parts[0];
-    var partialend = parts[1];
+var numCPUs = 2;
 
-    var start = parseInt(partialstart, 10);
-    var end = partialend ? parseInt(partialend, 10) : total-1;
-    var chunksize = (end-start)+1;
-    console.log('RANGE: ' + start + ' - ' + end + ' = ' + chunksize);
-
-    var file = fs.createReadStream(path, {start: start, end: end});
-    res.writeHead(206, { 'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
-    file.pipe(res);
-  } else {
-    console.log('ALL: ' + total);
-    res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'video/mp4' });
-    fs.createReadStream(path).pipe(res);
+if (cluster.isMaster) {
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
   }
-}).listen(1337);
+}
+else {
+  http.createServer(function (req, res) {
+    var path = '../storage/app/public/videos/1-Welcome%20to%20Republic%20City.%20A%20Leaf%20in%20the%20Wind.mp4';
+    var stat = fs.statSync(path);
+    var total = stat.size;
+    if (req.headers['range']) {
+      var range = req.headers.range;
+      var parts = range.replace(/bytes=/, "").split("-");
+      var partialstart = parts[0];
+      var partialend = parts[1];
+      var start = parseInt(partialstart, 10);
+      var end = partialend ? parseInt(partialend, 10) : total-1;
+      var chunksize = (end-start)+1;
+
+      var file = fs.createReadStream(path, {start: start, end: end});
+      res.writeHead(206, { 'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
+      file.pipe(res);
+    }
+    else {
+      res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'video/mp4' });
+      fs.createReadStream(path).pipe(res);
+    }
+  }).listen(1337);
+}
