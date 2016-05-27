8554e796a35d2748a2fb82a112aff5b76667c5d0
Andrew Mikhailov <Andrew.Mikhailov.18.01@gmail.com>
2016-05-28 00:00:19 +0600

    Stylish login page.

--- admin/resources/views/login.blade.php
+++ admin/resources/views/login.blade.php
@@ -1,40 +1,33 @@
 @extends('layouts.main')
 @section('title', 'Admin login')
-@section('body-class', 'login-page')
+@section('body-class', 'new-login-page')
 @section('content')
-    <div class="container">
-        <div class="login-box">
-            <div>
-                <div class="login-form row">
-                    <div class="col-sm-12 text-center login-header">
-                        <i class="login-logo fa fa-connectdevelop fa-5x"></i>
-                        <h4 class="login-title">Admin</h4>
-                    </div>
-                    <div class="col-sm-12">
-                        <div class="login-body">
-                            <div class="progress hidden" id="login-progress">
-                                <div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
-                                    Log In...
-                                </div>
-                            </div>
-                            {{ Form::open(array('url' => 'admin/login')) }}
-                                {!! csrf_field() !!}
-                                <div class="control">
-                                    {{ $errors->first('email') }}
-                                    {{ Form::text('email', Input::old('email'), array('class' => 'form-control', 'placeholder' => 'Email')) }}
-                                </div>
-                                <div class="control">
-                                    {{ $errors->first('password') }}
-                                    {{ Form::password('password', array('id' => 'password', 'class' => 'form-control', 'placeholder' => 'Password')) }}
-                                </div>
-                                <div class="login-button text-center">
-                                    {{ Form::submit('Login', array('class' => 'btn btn-primary')) }}
-                                </div>
-                            {{ Form::close() }}
-                        </div>
-                    </div>
+    <div id="large-header" class="large-header">
+        <canvas id="bg-canvas"></canvas>
+        <div id="warp">
+            ADMIN DASHBOARD
+            {{ Form::open(array('url' => 'admin/login')) }}
+            {!! csrf_field() !!}
+            <div class="admin">
+                <div class="rota">
+                    <h1>ADMIN</h1>
+                    {{ $errors->first('email') }}
+                    {{ Form::text('email', Input::old('email'), array('class' => 'form-control', 'placeholder' => 'Email')) }}
+                    {{ $errors->first('password') }}
+                    {{ Form::password('password', array('id' => 'password', 'class' => 'form-control', 'placeholder' => 'Password')) }}
                 </div>
             </div>
+            <div class="cms">
+                <div class="roti">
+                    <h1>CMS</h1>
+                    {{ Form::submit(
+                        'Login',
+                         array('id' => 'valid')
+                     )}}<br />
+                    {{--<p><a href="#">Register</a> <a href="#">Forgot Password</a> <a href="#">Help</a></p>--}}
+                </div>
+            </div>
+            {{ Form::close() }}
         </div>
     </div>
 @stop
