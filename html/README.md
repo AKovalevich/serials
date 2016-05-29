dac03a9d0700bf2b9191895cbe87e244939f5f11
Andrew Mikhailov <Andrew.Mikhailov.18.01@gmail.com>
2016-05-29 18:38:45 +0600

    Update html.

---
+++ html/README.md
@@ -0,0 +1,1 @@
+# andrew-mikhailov.github.io
--- html/js/main.js
+++ html/js/main.js
@@ -1,29 +1,34 @@
 $(document).ready(function(){
-    $('.grid').slick({
-        dots: false,
+    $('.grid-carousel').slick({
+        dots: true,
         infinite: true,
         speed: 300,
-//        slidesToShow: 1,
-//        centerMode: true,
-        variableWidth: true
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        variableWidth: true,
+        centerMode: false,
+        focusOnSelect: false
     });
 
-    $('.more-info-button img').click(function () {
-        var $this = $(this);
-        var infoBlock = $(".carousel-info-block", $this.closest('.carousel-block'));
-        $(infoBlock).addClass('show');
-        $('html, body').animate({
-            scrollTop: $this.closest('.carousel-block').find('.block-title').offset().top
-        }, 700);
+    $('.main-carousel').slick({
+        swipe: false
     });
 
-    $('.main-menu .menu__list .menu__item').click(function () {
-        $('.main-menu .menu__list .menu__item').removeClass('menu__item--current');
-        $(this).addClass('menu__item--current');
-    });
-    $('.info-block-menu .menu__list .menu__item').click(function () {
-        $('.info-block-menu .menu__list .menu__item').removeClass('menu__item--current');
-        $(this).addClass('menu__item--current');
-    });
+//    $('.more-info-button img').click(function () {
+//        var $this = $(this);
+//        var infoBlock = $(".carousel-info-block", $this.closest('.carousel-block'));
+//        $(infoBlock).addClass('show');
+//        $('html, body').animate({
+//            scrollTop: $this.closest('.carousel-block').find('.block-title').offset().top
+//        }, 700);
+//    });
+//
+//    $('.main-menu .menu__list .menu__item').click(function () {
+//        $('.main-menu .menu__list .menu__item').removeClass('menu__item--current');
+//        $(this).addClass('menu__item--current');
+//    });
+//    $('.info-block-menu .menu__list .menu__item').click(function () {
+//        $('.info-block-menu .menu__list .menu__item').removeClass('menu__item--current');
+//        $(this).addClass('menu__item--current');
+//    });
 });
-
