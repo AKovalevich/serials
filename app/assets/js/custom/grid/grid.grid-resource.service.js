f7211fc01dcfa557d0ed3ecd2bb5ef23bc99bf31
Andrew Mikhailov <Andrew.Mikhailov.18.01@gmail.com>
2016-06-02 23:51:32 +0600

    Improve gallery.

--- app/assets/js/custom/grid/grid.grid-resource.service.js
+++ app/assets/js/custom/grid/grid.grid-resource.service.js
@@ -14,60 +14,70 @@
           return [
             {
               id: 'content1',
+              title: 'Vikings',
               preview: '/assets/images/placeholders/vik.jpg',
               full: '/assets/images/placeholders/vik.jpg',
               text: 'content1'
             },
             {
               id: 'content2',
+              title: 'Big Bang Theory',
               preview: '/assets/images/placeholders/tt.jpg',
               full: '/assets/images/placeholders/tt.jpg',
               text: 'content2'
             },
             {
               id: 'content3',
+              title: 'Game of Thrones',
               preview: '/assets/images/placeholders/dd.jpg',
               full: '/assets/images/placeholders/dd.jpg',
               text: 'content3'
             },
             {
               id: 'content4',
+              title: '100',
               preview: '/assets/images/placeholders/100.jpg',
               full: '/assets/images/placeholders/100.jpg',
               text: 'content4'
             },
             {
               id: 'content5',
+              title: 'Walking Dead',
               preview: '/assets/images/placeholders/wd.jpg',
               full: '/assets/images/placeholders/wd.jpg',
               text: 'content5'
             },
             {
               id: 'content6',
+              title: 'Some TV Show',
               preview: '/assets/images/placeholders/ftd.jpg',
               full: '/assets/images/placeholders/ftd.jpg',
               text: 'content6'
             },
             {
               id: 'content7',
+              title: 'Walking Dead',
               preview: '/assets/images/placeholders/wd.jpg',
               full: '/assets/images/placeholders/wd.jpg',
               text: 'content7'
             },
             {
               id: 'content8',
+              title: 'Some TV Show',
               preview: '/assets/images/placeholders/ftd.jpg',
               full: '/assets/images/placeholders/ftd.jpg',
               text: 'content8'
             },
             {
               id: 'content9',
+              title: 'House M.D.',
               preview: '/assets/images/placeholders/dh.jpg',
               full: '/assets/images/placeholders/dh.jpg',
               text: 'content9'
             },
             {
               id: 'content10',
+              title: 'Vikings',
               preview: '/assets/images/placeholders/vik.jpg',
               full: '/assets/images/placeholders/vik.jpg',
               text: 'content10'
